d97f78d16beb894d563a07b502c611a08815f12a
Kevin Rivera <kevinsantiagoriverarestrepo@gmail.com>
2022-06-09 04:30:14 -0500

    update de cambios

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,65 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <div class="navbar-container">
+      <div class="navbar-content">
+        <div class="icons">
+          <router-link active to="/">
+            <i class="bx bx-home-alt bx-sm"></i>
+          </router-link>
+          <router-link active to="/cesta">
+            <i class="bx bx-trash bx-sm"></i>
+          </router-link>
+          <router-link active  to="/favoritos">
+            <i class="bx bx-heart bx-sm"></i>
+          </router-link>
+          <router-link active to="/historial">
+            <i class='bx bx-sync bx-sm'></i>
+          </router-link>
+        </div>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+
+.navbar-container {
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+}
+.navbar-content {
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+}
+
+a {
+  padding: 27px;
+  color: rgba(163, 163, 163);
+}
+
+a:hover {
+  color: #845EC2;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+* {
+    font-family: 'Josefin Sans', sans-serif;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #845EC2;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,248 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header">
+      <h2 class="title">Hi <b>Businessman!</b></h2>
+      <div class="input">
+        <input type="text" placeholder="Busca tus productos">
+        <div class="icon-input">
+          <i class="bx bx-search"></i>
+        </div>
+      </div>
+      <div class="combo-header">
+        <div class="text">
+          <h3>Productos en promoción</h3>
+          <br>
+          <p>Miles de descuentos para los ususarios</p>
+        </div>
+        <div class="img">
+          <img src="https://images.unsplash.com/photo-1462392246754-28dfa2df8e6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="scroll-menu">
+      <div class="scroll-content">
+        <div class="avatar">
+          <i class='bx bx-laptop bx-sm icono'></i>
+        </div>
+        <div class="avatar">
+          <i class='bx bx-cart-add bx-sm icono'></i> 
+        </div>
+        <div class="avatar">
+          <i class='bx bxs-hard-hat bx-sm icono'></i>
+        </div>
+        <div class="avatar">
+          <i class='bx bxs-dog bx-sm icono'></i>
+        </div>
+        <div class="avatar">
+          <i class='bx bxs-speaker bx-sm icono'></i>
+        </div>
+        <div class="avatar">
+          <i class='bx bxs-wine bx-sm icono'></i>
+        </div>
+        <div class="avatar">
+          <i class='bx bx-joystick bx-sm icono'></i>
+        </div>
+        <div class="avatar">
+          <i class='bx bx-car bx-sm icono'></i>
+        </div>
+        <div class="avatar">
+          <i class='bx bxs-watch-alt bx-sm icono'></i>
+        </div>
+        <div class="avatar">
+          <i class='bx bxs-wrench bx-sm icono'></i>
+        </div>
+      </div>
+    </div>
+    <br><br>
+    <h2>Productos de calidad</h2>
+    <div class="scroll-menu">
+      <div class="scroll-content">
+        <div class="top-products">
+          <div class="top-products-img">
+            <img class="img2"  src="https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+          </div>
+    
+            <div class="top-products-text">
+            <div class="top-products-title">Tennis Reebok</div>
+            <div class="top-products-price">$50.000 CO</div>  
+            </div>
+              <i class="bx bx-plus-medical icono2"></i>
+        </div>
+        <div class="top-products">
+          <div class="top-products-img">
+            <img class="img2"  src="https://images.unsplash.com/photo-1567361808960-dec9cb578182?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1195&q=80" alt="">
+          </div>
+    
+            <div class="top-products-text">
+            <div class="top-products-title">Herramienta</div>
+            <div class="top-products-price">$70.000 CO</div>  
+            </div>
+              <i class="bx bx-plus-medical icono2"></i>
+        </div>
+        <div class="top-products">
+          <div class="top-products-img">
+            <img class="img2"  src="https://images.unsplash.com/photo-1598986646512-9330bcc4c0dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+          </div>
+    
+            <div class="top-products-text">
+            <div class="top-products-title">Set-up</div>
+            <div class="top-products-price">$2.000 USD</div>  
+            </div>
+              <i class="bx bx-plus-medical icono2"></i>
+        </div>
+        <div class="top-products">
+          <div class="top-products-img">
+            <img class="img2"  src="https://images.unsplash.com/photo-1566576912321-d58ddd7a6088?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+          </div>
+    
+            <div class="top-products-text">
+            <div class="top-products-title">Juguetes</div>
+            <div class="top-products-price">$100.000 CO</div>  
+            </div>
+              <i class="bx bx-plus-medical icono2"></i>
+        </div>
+        <div class="top-products">
+          <div class="top-products-img">
+            <img class="img2"  src="https://images.unsplash.com/photo-1600861194942-f883de0dfe96?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" alt="">
+          </div>
+    
+            <div class="top-products-text">
+            <div class="top-products-title">Controles</div>
+            <div class="top-products-price">$170.000 CO</div>  
+            </div>
+              <i class="bx bx-plus-medical icono2"></i>
+        </div>
+        <div class="top-products">
+          <div class="top-products-img">
+            <img class="img2"  src="https://images.unsplash.com/photo-1557149559-d74af2d38a1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+          </div>
+    
+            <div class="top-products-text">
+            <div class="top-products-title">Licor</div>
+            <div class="top-products-price">$220.000 CO</div>  
+            </div>
+              <i class="bx bx-plus-medical icono2"></i>
+        </div>
+      </div>
+    </div>
+    <br><br>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+<style>
+  .scroll-content-top {
+    display: flex;
   }
-}
-</script>
+  .scroll-menu-top {
+  }
+  .top-products-img {
+    width: 100%;
+  } 
+  .img2 {
+    width: 100%;
+  }
+  .card-footer {
+    display: flex;
+  }
+  .icono2 {
+    padding: 10px;
+    color: #fff;
+    border-radius: 50px;
+    background-color: #008F7A;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    margin: 5px;
+  }
+  .top-products-text {
+    margin-top: 10px;
+  }
+  .top-products-price {
+    color: #008E9B;
+  }
+  .top-products-title {
+    color: #2C73D2;
+  }
+  .top-products {
+    padding: 10px;
+    border-radius: 10px;
+    min-width: 150px;
+    position: relative;
+  }
+  .home {
+    margin: 20px;
+  }
+  .title {
+    font-weight: 350;
+  }
+  .input {
+    width: 100%;
+    display: flex;
+    position: relative;
+    align-items: center;
+    margin-top: 15px;
+  }
+  input {
+    width: 100%;
+    background-color: rgba(250, 248, 248);
+    border: 0px;
+    outline: none;
+    padding: 10px;
+    padding-left: 30px;
+    border-radius: 10px;  
+  }
+  .icon-input {
+    position: absolute;
+    margin-left: 10px;
+  }
+  .combo-header {
+    background-color: #0081CF;
+    color: #fff;
+    padding: 10px;
+    border-radius: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 20px;
+  }
+  h3 {
+    font-weight: 400;
+  }
+  p {
+    font-weight: 300;
+  }
+  .text {
+    width: 50%;
+  }
+  .img {
+    width: 50%;
+  }
+  img {
+    width: 100%;
+    object-fit: cover;
+    border-radius: 10px;
+  }
+  .scroll-menu {
+    margin-top: 10px;
+  }
+  .scroll-content {
+    display: flex;
+    overflow-y: auto;
+  }
+  .scroll-content::-webkit-scrollbar{
+    display: none;
+  }
+  .avatar {
+    background-color: rgba(250, 248, 248);
+    padding: 20px;
+    border-radius: 10px;
+    margin: 2px;
+  }
+  .icono {
+    color: #008E9B;
+  }
+
+  h2 {
+    font-weight: 400;
+  }
+</style>
